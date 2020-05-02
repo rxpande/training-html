4322f64c90caa4576dec008ef053cbb6381bcbe0
rxpande <rxpande@gmail.com>
2020-05-02 19:54:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# training-html-base-template-with-typescript-3qrgpz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/training-html-base-template-with-typescript-3qrgpz)
---
+++ index.ts
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>HTML Base Template for All Training with TypeScript</h1>`;
